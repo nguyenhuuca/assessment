343609ed3eb8048081f93df861875869078405cb
nguyenhuuca <nguyenhuuca@gmail.com>
2025-06-04 22:15:53 +0700

    update using password less

--- webapp/js/assessment.js
+++ webapp/js/assessment.js
@@ -1,4 +1,8 @@
-
+let appConst = {
+    // baseUrl: "http://localhost:8081/v1/funny-app",
+    baseUrl: "https://canh-labs.com/api/v1/funny-app",
+    offlineMode: false  // Flag to control offline/online mode
+}
 /**
  * Using to holed video object
  * @param {*} id : id video after share
