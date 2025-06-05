6776d4ab1ee0289f93a0261f649c7e7cf130e92f
nguyenhuuca <nguyenhuuca@gmail.com>
2025-06-05 09:23:43 +0700

    update fe

--- webapp/js/common.js
+++ webapp/js/common.js
@@ -23,3 +23,30 @@ function showMessage(message, type = 'error') {
     $msg.text(message).show();
 }
 
+$(document).ready(function() {
+    // Get full query string, e.g. "?=abcsxs" or "?token=abcsxs"
+    let queryString = window.location.search; // returns "?=abcsxs"
+
+    // incase  param key=value like ?token=abc the using URLSearchParams:
+    let params = new URLSearchParams(queryString);
+
+    // incase token is param 'token'
+    let token = params.get('token');
+
+    // incase URL is ?=abcsxs (no key), have to pasre it:
+    if (!token) {
+        // queryString = "?=abcsxs"
+        // remove "?" và "="
+        token = queryString.replace(/^(\?|=)/g, '');
+    }
+
+    console.log("Token from URL:", token);
+
+    if(token) {
+        // call other api
+        // $.post('/api/verify-token', { token: token }, function(response) { ... })
+    } else {
+        console.error("Token not found in URL!");
+    }
+});
+
