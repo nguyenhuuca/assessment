8467635b279945ea31503d5112cc77460cf7faf1
nguyenhuuca <nguyenhuuca@gmail.com>
2025-07-22 15:47:12 +0700

    Add logging to VideoAccessServiceImpl and comment out access recording logic

--- api/src/main/java/com/canhlabs/funnyapp/service/impl/VideoAccessServiceImpl.java
+++ api/src/main/java/com/canhlabs/funnyapp/service/impl/VideoAccessServiceImpl.java
@@ -3,6 +3,7 @@ package com.canhlabs.funnyapp.service.impl;
 import com.canhlabs.funnyapp.domain.VideoAccessStats;
 import com.canhlabs.funnyapp.repo.VideoAccessStatsRepo;
 import com.canhlabs.funnyapp.service.VideoAccessService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,7 @@ import java.time.Instant;
 import java.util.List;
 
 @Service
+@Slf4j
 public class VideoAccessServiceImpl implements VideoAccessService {
     private final VideoAccessStatsRepo repo;
     public VideoAccessServiceImpl(VideoAccessStatsRepo repo) {
@@ -21,10 +23,11 @@ public class VideoAccessServiceImpl implements VideoAccessService {
     @Async
     @Override
     public void recordAccess(String videoId) {
-        VideoAccessStats stats = repo.findByVideoId(videoId)
-                .orElseGet(() -> VideoAccessStats.builder().videoId(videoId).build());
-        stats.incrementHit();
-        repo.save(stats);
+        // Todo: will be implemented later after we create new db cluster
+//        VideoAccessStats stats = repo.findByVideoId(videoId)
+//                .orElseGet(() -> VideoAccessStats.builder().videoId(videoId).build());
+//        stats.incrementHit();
+//        repo.save(stats);
     }
 
     @Override
